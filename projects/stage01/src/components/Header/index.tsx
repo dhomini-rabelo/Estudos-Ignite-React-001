a4f1e0eeffd2d34cd242e1df8973d3cb09f8505a
dhomini-rabelo <dhominisalgado@gmail.com>
2022-08-20 21:34:59 -0300

    use header class

--- projects/stage01/src/components/Header/index.tsx
+++ projects/stage01/src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import logo from '../../assets/logo.svg'
 
 export function Header() {
     return (
-        <header>
+        <header className="header">
             <img src={logo} alt="ignite-logo" />
             <h1><strong>Ignite Feed</strong></h1>
         </header>
