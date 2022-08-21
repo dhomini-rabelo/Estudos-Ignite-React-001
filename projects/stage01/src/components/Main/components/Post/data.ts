5e1f9347414053804836ba28a05fa843a7cddb24
dhomini-rabelo <dhominisalgado@gmail.com>
2022-08-21 19:50:29 -0300

    add users list

--- projects/stage01/src/components/Main/components/Post/data.ts
+++ projects/stage01/src/components/Main/components/Post/data.ts
@@ -1,14 +1,11 @@
+import { currentUser, diegoUser } from "../../../../core/users";
 import { postType } from "./types";
 
 
 export let posts: postType[] = [
     {
         id: 1,
-        author: {
-            avatarUrl: 'https://images.unsplash.com/profile-1546504431541-6cc83b84050d?dpr=1&auto=format&fit=crop&w=150&h=150&q=60&crop=faces&bg=fff',
-            name: 'Michael Baccin',
-            role: 'Web Developer',
-        },
+        author: currentUser,
         content: [
             {tag: 'p', text: 'Fala galeraa 👋'},
             {tag: 'p', text: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
@@ -18,11 +15,7 @@ export let posts: postType[] = [
     },
     {
         id: 2,
-        author: {
-            avatarUrl: 'https://github.com/diego3g.png',
-            name: 'Diego Fernandes',
-            role: 'CEO @Rocketseat',
-        },
+        author: diegoUser,
         content: [
             {tag: 'p', text: 'Falaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa  dev.....'},
             {tag: 'p', text: 'Novo projeto postado no Ignite 🚀'},
---
+++ projects/stage01/src/core/users.tsx
@@ -0,0 +1,21 @@
+import { avatarType } from "../components/utils/Avatar/types";
+
+
+export const currentUser: avatarType = {
+    avatarUrl: 'https://images.unsplash.com/profile-1546504431541-6cc83b84050d?dpr=1&auto=format&fit=crop&w=150&h=150&q=60&crop=faces&bg=fff',
+    name: 'Michael Baccin',
+    role: 'Web Developer',
+}
+
+export const diegoUser: avatarType = {
+    avatarUrl: 'https://github.com/diego3g.png',
+    name: 'Diego Fernandes',
+    role: 'CEO @Rocketseat',
+}
+
+
+export const leslieUser: avatarType = {
+    avatarUrl: 'https://images.unsplash.com/profile-1453904936751-28af93873ed7?dpr=1&auto=format&fit=crop&w=150&h=150&q=60&crop=faces&bg=fff',
+    name: 'Leslie Alexander',
+    role: 'Mobile Developer',
+}
