7c908c118e43a4a7fa917edc4cd931f5b233728f
dhomini-rabelo <dhominisalgado@gmail.com>
2022-08-24 20:11:23 -0300

    ad project colors

--- projects/practice01/src/App.tsx
+++ projects/practice01/src/App.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 export function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline bg-purple-400">
+    <h1 className="text-3xl font-bold underline bg-pRed-300">
       Hello world!
     </h1>
   )
