05276665b247b16dc2431d4a1de34aea8cdac0e5
dhomini-rabelo <dhominisalgado@gmail.com>
2022-08-25 20:42:08 -0300

    move Header component

--- projects/practice01/src/App/index.tsx
+++ projects/practice01/src/App/index.tsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
-import { Header } from '../components/Header'
 import { Main } from '../components/Main/index';
+import logo from '../assets/images/logo.svg'
+import { Div } from './styles';
 
 
 export function App() {
 
   return (
     <div className="container mx-auto">
-      <Header />
+      <header>
+        <Div.imageContainer>
+            <img src={logo} alt="project-logo" className="mx-auto" />
+        </Div.imageContainer>
+      </header>
       <Main />
     </div>
   )
--- projects/practice01/src/components/Header/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-
-export const Div = {
-    imageContainer: styled.div`
-        margin-top: 4.5rem;
-    `,
-}
