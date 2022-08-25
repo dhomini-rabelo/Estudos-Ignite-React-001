e37934c89a7e468e3b3ec58e2f9f1b31b068be7d
dhomini-rabelo <dhominisalgado@gmail.com>
2022-08-25 20:38:42 -0300

    rn App to folder

--- projects/practice01/src/App/index.tsx
+++ projects/practice01/src/App/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import { Header } from './components/Header'
-import { Main } from './components/Main/index';
+import { Header } from '../components/Header'
+import { Main } from '../components/Main/index';
 
 
 export function App() {
