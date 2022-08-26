35807ce1b69131c14a0e9771555d9400f465b77c
dhomini-rabelo <dhominisalgado@gmail.com>
2022-08-25 21:29:01 -0300

    show task text

--- projects/practice01/src/components/Tasks/components/Task/index.tsx
+++ projects/practice01/src/components/Tasks/components/Task/index.tsx
@@ -1,11 +1,12 @@
 import { Circle, Trash } from "phosphor-react";
+import { taskType } from './../../../../types/tasks';
 
-export function Task() {
+export function Task({task}: {task: taskType}) {
     
     return (
         <div className="flex justify-between items-start py-4 px-4">
             <span className="cursor-pointer text-pBlue-400 hover:text-pBlue-800"><Circle size={24} /></span>
-            <span className="grow px-3">Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</span>
+            <span className="grow px-3">{task.text}</span>
             <span className="cursor-pointer text-pGray-300 hover:text-pRed-300"><Trash size={24} /></span>
         </div>
     )
