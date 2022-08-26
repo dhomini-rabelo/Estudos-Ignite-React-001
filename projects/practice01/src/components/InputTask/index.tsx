832fe2d4837141f898181d3ad729f9afa753d4cd
dhomini-rabelo <dhominisalgado@gmail.com>
2022-08-26 20:32:10 -0300

    ad feature to save task with Enter

--- projects/practice01/src/components/InputTask/index.tsx
+++ projects/practice01/src/components/InputTask/index.tsx
@@ -1,5 +1,5 @@
 import { PlusCircle } from 'phosphor-react'
-import { useRef } from 'react'
+import { useRef, KeyboardEvent } from 'react'
 import { Div } from './styles'
 
 
@@ -11,11 +11,16 @@ export function InputTask({ addTask }: { addTask: (text: string) => void }) {
     let newTaskText = input.current!.value.slice()
     addTask(newTaskText)
     input.current!.value = ''
+    input.current!.focus()
+  }
+
+  function handleNewTaskByKeyboard(e: KeyboardEvent<HTMLInputElement>) {
+    if (e.key === "Enter") {handleNewTask()}
   }
 
   return (
     <Div.container className="flex justify-between items-stretch">
-      <input type="text" name="task" id="task" placeholder="Adicione uma tarefa" ref={input}
+      <input type="text" name="task" id="task" placeholder="Adicione uma tarefa" ref={input} onKeyDown={handleNewTaskByKeyboard}
         className="flex-1 p-4 bg-pGray-500 border-solid border-pGray-700 rounded-lg mr-2 placeholder-pGray-300 text-pGray-100" 
       />
       <button onClick={handleNewTask} className="bg-pBlue-800 hover:bg-pBlue-400 p-4 text-pGray-100 text-sm rounded-lg"><span>Criar</span> <PlusCircle size={16} className="inline-block" /></button>
