13eb2d455d8bd185499e6858a02ffdb7d8138cdd
dhomini-rabelo <dhominisalgado@gmail.com>
2022-08-26 21:14:14 -0300

    ad X to close Modal

--- projects/practice01/src/components/Tasks/components/EditTaskModal/index.tsx
+++ projects/practice01/src/components/Tasks/components/EditTaskModal/index.tsx
@@ -1,4 +1,5 @@
 import { Dialog, Transition } from '@headlessui/react'
+import { X } from 'phosphor-react'
 import { Fragment, useState } from 'react'
 import { Div } from './styles'
 
@@ -58,7 +59,10 @@ export default function MyModal() {
                     as="h3"
                     className="text-lg font-medium leading-6 text-pGray-100"
                   >
-                    Editar tarefa
+                    <div className="flex justify-between items-center flex-wrap">
+                        <strong>Editar tarefa</strong>
+                        <span onClick={closeModal} className="cursor-pointer"><X size={24} weight="fill" /></span>
+                    </div>
                   </Dialog.Title>
                   <Div.inputContainer className="mt-6 w-full">
                   <input type="text" name="task" id="task" placeholder="Adicione uma tarefa"
